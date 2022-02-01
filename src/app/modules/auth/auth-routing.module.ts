bb974c292fbd1dbb1bfb40bab27e5c53ce9f70fd
Kishoreitprofound61 <kishore@itprofound.com>
2022-02-01 23:51:31 +0530

    signup page

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RegisterationComponent } from './registeration/registeration.component';
 
 
 const routes: Routes = [
   // { path: '', redirectTo: 'login', pathMatch: 'full' },
   {
       path: '', component: LoginComponent, children: [
-        { path: 'login', component: LoginComponent  }
+        { path: 'login', component: LoginComponent}
+        
         // { path: 'resetpassword', component: ResetPasswordComponent },
         // { path: 'forgotpassword', component: ForgotPasswordComponent }
       ]
   },
+  { path: 'register', component: RegisterationComponent}
   // { path: 'register', component: RegisterComponent }
 // {
 //   path:'/login',component:LoginComponent
