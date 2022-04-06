21718478e16a4b9dd413ce962bf7e060f2512fbf
revanthnani65 <revanthnani65@gmail.com>
2022-04-06 14:19:49 +0530

    profilepagechanges

--- src/app/modules/dashboard/profile/profile/profile.component.ts
+++ src/app/modules/dashboard/profile/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Subject } from 'rxjs';
 import { AuthServService } from 'src/app/services/auth-serv.service';
 import { EmployeeService } from 'src/app/services/employee.service';
@@ -11,28 +12,38 @@ import { EmployeeService } from 'src/app/services/employee.service';
 export class ProfileComponent implements OnInit {
   userData: any;
   User: any;
-  languages:string = ''
-  hobbies:string = ''
-  achievements:string =''
-  phoneNumber:any;
 
+  profileForm: FormGroup;
+  constructor(private authSer: AuthServService, private empSer: EmployeeService, private builder: FormBuilder,) {
+
+    this.userData = JSON.parse(this.authSer.tokenData());
+    this.empSer.userid.next(this.userData.id);
+    this.empSer.getEmployeeById(this.userData.id).subscribe((res: any) => {
+      this.User = Object.values(res.data);
+    })
+   
+      this.profileForm = builder.group({
+        phoneNumber: '',
+        faceBook: '',
+        languages: '',
+        achievements: '',
+        email: '',
+        hobbies:'',
   
-
-  constructor(private authSer:AuthServService, private empSer:EmployeeService) { 
-   this.userData = JSON.parse(this.authSer.tokenData());
-   this.empSer.userid.next(this.userData.id);
-   this.empSer.getEmployeeById(this.userData.id).subscribe((res:any)=>{   
-     this.User = Object.values(res.data);
-     console.log(this.User);
-   })
+      })
   }
 
   ngOnInit(): void {
-    
+
   }
 
-  save(){
-    console.log(this.languages)
-  }
+   profileSubmit(value: any) {
+     this.profileForm.value["empId"] = this.userData.id;
+    this.empSer.updateEmployee(value).subscribe((res:any)=>{
+      if(res){
+        console.log("details edited successfully")
+      }
+    })
+   }
 
 }
